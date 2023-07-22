f72ee298de466a33f72fc22b5610abbcb8bded43
Azeez Ridwan <readone.cybernet@gmail.com>
2023-07-22 12:36:35 +0100

    updates 1

---
+++ src/main/java/com/filesUploadAndDownloadApi/FilesUploadAndDownloadApiApplication.java
@@ -0,0 +1,13 @@
+package com.filesUploadAndDownloadApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesUploadAndDownloadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesUploadAndDownloadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filesUploadAndDownloadApi/FilesUploadAndDownloadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filesUploadAndDownloadApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesUploadAndDownloadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
