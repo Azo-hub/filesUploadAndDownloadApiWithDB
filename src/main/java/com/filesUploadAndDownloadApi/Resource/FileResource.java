dc493c3f3cb8e91910924fc764f7a494bb71d936
Azeez Ridwan <readone.cybernet@gmail.com>
2023-08-07 20:43:50 +0100

    update 1.1

--- src/main/java/com/filesUploadAndDownloadApi/Resource/FileResource.java
+++ src/main/java/com/filesUploadAndDownloadApi/Resource/FileResource.java
@@ -1,5 +1,6 @@
 package com.filesUploadAndDownloadApi.Resource;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,8 +9,14 @@ import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,6 +44,26 @@ public class FileResource {
 
 		return ResponseEntity.ok().body(filenames);
 
+	}
+	
+	//Define a method to download files
+	@GetMapping("/download/{filename}")
+	public ResponseEntity<Resource> downloadFiles(@PathVariable("filename") String filename) throws IOException {
+
+		Path filePath = Paths.get(DIRECTORY).toAbsolutePath().normalize().resolve(filename);
+
+		if (!Files.exists(filePath)) {
+			throw new FileNotFoundException(filename + "was not found on the server");
+		}
+
+		Resource resource = new UrlResource(filePath.toUri());
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("File-Name", filename);
+		httpHeaders.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;File-Name=" + resource.getFilename());
+
+		return ResponseEntity.ok().contentType(MediaType.parseMediaType(Files.probeContentType(filePath)))
+				.headers(httpHeaders).body(resource);
 	}
 
 }
